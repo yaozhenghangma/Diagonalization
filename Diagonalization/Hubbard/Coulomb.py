41e8803d6278fd9aace0d96c25c71480503d1e8a
YaozhenghangMa <Yaozhenghang.Ma@gmail.com>
2023-11-16 22:03:12 +0800

    update

--- Diagonalization/Hubbard/Coulomb.py
+++ Diagonalization/Hubbard/Coulomb.py
@@ -1,54 +1,54 @@
 import numpy as np
 
-def IntraOrbital(num_sites, num_orbs_per_site, total_orbs):
+def IntraOrbital(num_sites, num_orbs_per_site, total_orbs, shift=0):
     repulsion_list = []
     for i in range(0, num_orbs_per_site):
         for j in range(0, num_sites):
-            repulsion_list.append({i+j*num_orbs_per_site, i+j*num_orbs_per_site+total_orbs})
+            repulsion_list.append({i+j*num_orbs_per_site+shift, i+j*num_orbs_per_site+total_orbs+shift})
     return repulsion_list
 
-def InterOrbital(num_sites, num_orbs_per_site, total_orbs):
+def InterOrbital(num_sites, num_orbs_per_site, total_orbs, shift=0):
     repulsion_list = []
     for i in range(0, num_orbs_per_site):
         for j in range(i+1, num_orbs_per_site):
             for k in range(0, num_sites):
-                repulsion_list.append({i+k*num_orbs_per_site, j+k*num_orbs_per_site+total_orbs})
-                repulsion_list.append({j+k*num_orbs_per_site, i+k*num_orbs_per_site+total_orbs})
+                repulsion_list.append({i+k*num_orbs_per_site+shift, j+k*num_orbs_per_site+total_orbs+shift})
+                repulsion_list.append({j+k*num_orbs_per_site+shift, i+k*num_orbs_per_site+total_orbs+shift})
     return repulsion_list
 
-def InterOrbitalHund(num_sites, num_orbs_per_site, total_orbs):
+def InterOrbitalHund(num_sites, num_orbs_per_site, total_orbs, shift=0):
     repulsion_list = []
     for i in range(0, num_orbs_per_site):
         for j in range(i+1, num_orbs_per_site):
             for k in range(0, num_sites):
-                repulsion_list.append({i+k*num_orbs_per_site, j+k*num_orbs_per_site})
-                repulsion_list.append({i+k*num_orbs_per_site+total_orbs, j+k*num_orbs_per_site+total_orbs})
+                repulsion_list.append({i+k*num_orbs_per_site+shift, j+k*num_orbs_per_site+shift})
+                repulsion_list.append({i+k*num_orbs_per_site+total_orbs+shift, j+k*num_orbs_per_site+total_orbs+shift})
     return repulsion_list
 
-def IntraOrbitalHoppingHund(num_sites, num_orbs_per_site, total_orbs):
+def IntraOrbitalHoppingHund(num_sites, num_orbs_per_site, total_orbs, shift=0):
     annihilation_list = []
     creation_list = []
     for i in range(0, num_orbs_per_site):
         for j in range(i+1, num_orbs_per_site):
             for k in range(0, num_sites):
-                annihilation_list.append({i+k*num_orbs_per_site, j+k*num_orbs_per_site+total_orbs})
-                creation_list.append({i+k*num_orbs_per_site+total_orbs, j+k*num_orbs_per_site})
+                annihilation_list.append({i+k*num_orbs_per_site+shift, j+k*num_orbs_per_site+total_orbs+shift})
+                creation_list.append({i+k*num_orbs_per_site+total_orbs+shift, j+k*num_orbs_per_site+shift})
 
-                annihilation_list.append({j+k*num_orbs_per_site, i+k*num_orbs_per_site+total_orbs})
-                creation_list.append({j+k*num_orbs_per_site+total_orbs, i+k*num_orbs_per_site})
+                annihilation_list.append({j+k*num_orbs_per_site+shift, i+k*num_orbs_per_site+total_orbs+shift})
+                creation_list.append({j+k*num_orbs_per_site+total_orbs+shift, i+k*num_orbs_per_site+shift})
 
     return annihilation_list, creation_list
 
-def InterOrbitalHoppingHund(num_sites, num_orbs_per_site, total_orbs):
+def InterOrbitalHoppingHund(num_sites, num_orbs_per_site, total_orbs, shift=0):
     annihilation_list = []
     creation_list = []
     for i in range(0, num_orbs_per_site):
         for j in range(i+1, num_orbs_per_site):
             for k in range(0, num_sites):
-                annihilation_list.append({i+k*num_orbs_per_site, i+k*num_orbs_per_site+total_orbs})
-                creation_list.append({j+k*num_orbs_per_site+total_orbs, j+k*num_orbs_per_site})
+                annihilation_list.append({i+k*num_orbs_per_site+shift, i+k*num_orbs_per_site+total_orbs+shift})
+                creation_list.append({j+k*num_orbs_per_site+total_orbs+shift, j+k*num_orbs_per_site+shift})
 
-                annihilation_list.append({j+k*num_orbs_per_site, j+k*num_orbs_per_site+total_orbs})
-                creation_list.append({i+k*num_orbs_per_site+total_orbs, i+k*num_orbs_per_site})
+                annihilation_list.append({j+k*num_orbs_per_site+shift, j+k*num_orbs_per_site+total_orbs+shift})
+                creation_list.append({i+k*num_orbs_per_site+total_orbs+shift, i+k*num_orbs_per_site+shift})
 
     return annihilation_list, creation_list
